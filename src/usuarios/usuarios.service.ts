51a41bdddda7c9852e8f4fc1223219fd6e49d709
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-24 12:24:57 -0300

    Update com base no modelo do front

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -23,16 +23,16 @@ export class UsuariosService {
     ) {}
 
     async create(modelo: ModeloCadastroUsuarioPerfil) {
-        const usuarioExistente = await this.usuariosRepository.find({
+        const usuarioExistente = await this.usuariosRepository.findOne({
             where: [{nomeUser: modelo.usuario.nomeUser}],
         });
 
-        if (usuarioExistente.length > 0) {
+        if (usuarioExistente) {
             throw new ConflictException("Usuário já existe no banco de dados");
         }
 
         const usuarioNovo = modelo.usuario;
-        this.usuariosRepository.save(usuarioNovo);
+        await this.usuariosRepository.save(usuarioNovo);
 
         // Vou forçar o front a enviar apenas o ID de cada perfil, portanto essa busca aqui é necessária
         const perfisId = modelo.perfisUsuario.map((perfil) => perfil.id);
@@ -57,6 +57,9 @@ export class UsuariosService {
 
     findOne(id: number) {
         return this.usuariosRepository.findOne({
+            relations: {
+                usuarioPermissoes: true,
+            },
             where: {
                 id,
             },
@@ -64,9 +67,46 @@ export class UsuariosService {
     }
 
     async update(id: number, modelo: ModeloCadastroUsuarioPerfil) {
+        const usuarioBanco = await this.usuariosRepository.findOne({
+            relations: {
+                usuarioPermissoes: true,
+            },
+            where: {
+                id,
+            },
+        });
+
+        if (!usuarioBanco) {
+            throw new BadRequestException(
+                "Usuário não encontrado no banco de dados"
+            );
+        }
+
+        for (let i = 0; i < usuarioBanco.usuarioPermissoes.length; i++) {
+            this.usuarioPermissaoRepository.delete(
+                usuarioBanco.usuarioPermissoes[i].id
+            );
+        }
+
         modelo.usuario.id = id;
-        const novoUsuario = await this.usuariosRepository.save(modelo.usuario);
-        return novoUsuario;
+        const usuarioNovo = modelo.usuario;
+
+        await this.usuariosRepository.save(usuarioNovo);
+
+        const perfisId = modelo.perfisUsuario.map((perfil) => perfil.id);
+
+        for (let i = 0; i < perfisId.length; i++) {
+            const perfil = await this.perfilRepository.findOne({
+                where: {id: perfisId[i]},
+            });
+            const usuarioPermissao = new UsuarioPermissao();
+            usuarioPermissao.usuario = usuarioNovo;
+            usuarioPermissao.dataHora = new Date();
+            usuarioPermissao.perfil = perfil;
+            this.usuarioPermissaoRepository.save(usuarioPermissao);
+        }
+
+        return modelo;
     }
 
     async remove(id: number) {
