9b73ef6c3a999bab40911b6086bf0e7a3b84b068
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-08-25 15:58:14 -0300

    Prettier

--- src/perfil-permissao/perfil-permissao.controller.spec.ts
+++ src/perfil-permissao/perfil-permissao.controller.spec.ts
@@ -1,20 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PerfilPermissaoController } from './perfil-permissao.controller';
-import { PerfilPermissaoService } from './perfil-permissao.service';
+import {Test, TestingModule} from "@nestjs/testing";
+import {PerfilPermissaoController} from "./perfil-permissao.controller";
+import {PerfilPermissaoService} from "./perfil-permissao.service";
 
-describe('PerfilPermissaoController', () => {
-  let controller: PerfilPermissaoController;
+describe("PerfilPermissaoController", () => {
+    let controller: PerfilPermissaoController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [PerfilPermissaoController],
-      providers: [PerfilPermissaoService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [PerfilPermissaoController],
+            providers: [PerfilPermissaoService],
+        }).compile();
 
-    controller = module.get<PerfilPermissaoController>(PerfilPermissaoController);
-  });
+        controller = module.get<PerfilPermissaoController>(
+            PerfilPermissaoController
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(controller).toBeDefined();
+    });
 });
--- src/perfil-permissao/perfil-permissao.service.spec.ts
+++ src/perfil-permissao/perfil-permissao.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PerfilPermissaoService } from './perfil-permissao.service';
+import {Test, TestingModule} from "@nestjs/testing";
+import {PerfilPermissaoService} from "./perfil-permissao.service";
 
-describe('PerfilPermissaoService', () => {
-  let service: PerfilPermissaoService;
+describe("PerfilPermissaoService", () => {
+    let service: PerfilPermissaoService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PerfilPermissaoService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [PerfilPermissaoService],
+        }).compile();
 
-    service = module.get<PerfilPermissaoService>(PerfilPermissaoService);
-  });
+        service = module.get<PerfilPermissaoService>(PerfilPermissaoService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(service).toBeDefined();
+    });
 });
